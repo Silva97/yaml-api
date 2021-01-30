f369ab3e55b0eccb20e418bd3ef2cbf699214c08
Luiz Felipe <felipe.silva337@yahoo.com>
2021-01-30 19:53:11 -0300

    feat: implement DateFormat

---
+++ src/date-format.ts
@@ -0,0 +1,23 @@
+export class DateFormat {
+    date: Date;
+
+    constructor() {
+        this.date = new Date();
+    }
+
+    public getFullDate(separator: string = '-') {
+        return [
+            this.date.getUTCFullYear().toString().padStart(4, '0'),
+            (this.date.getUTCMonth() + 1).toString().padStart(2, '0'),
+            this.date.getUTCDate().toString().padStart(2, '0'),
+        ].join(separator);
+    }
+
+    public getFullTime(separator: string = ':') {
+        return [
+            this.date.getUTCHours().toString().padStart(2, '0'),
+            this.date.getUTCMinutes().toString().padStart(2, '0'),
+            this.date.getUTCSeconds().toString().padStart(2, '0'),
+        ].join(separator) + ' UTC';
+    }
+}
